8cfaf2297a32f131402ca889627d67dea8b057e7
DavidTran <dtrancpp@gmail.com>
2021-09-13 22:00:19 -0700

    add properties to PrizeModel

--- TournamentTracker/TrackerLibrary/PrizeModel.cs
+++ TournamentTracker/TrackerLibrary/PrizeModel.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace TrackerLibrary
 {
-    class PrizeModel
+    public class PrizeModel
     {
+        public int PlaceNumber { get; set; }
+        public String PlaceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+
     }
 }
