e3df233d582fe9f95f014a39aca59e9ef6d4a0fb
DavidTran <dtrancpp@gmail.com>
2021-09-13 21:53:21 -0700

    remove unnecesarry comments

--- TournamentTracker/TrackerLibrary/TeamModel.cs
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -6,11 +6,8 @@ namespace TrackerLibrary
 {
     public class TeamModel
     {
-        // holds data for List of Person
-        // by default initalize TeamMembers as empty list
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
 
-        // holds data for String TeamName
         public String TeamName { get; set; }
 
     }
