66b8e7c9fdf37c4a29a83a97b404707ac8556486
DavidTran <dtrancpp@gmail.com>
2021-09-14 12:20:43 -0700

    add XML comments to PersonModel class

--- TournamentTracker/TrackerLibrary/PersonModel.cs
+++ TournamentTracker/TrackerLibrary/PersonModel.cs
@@ -4,11 +4,29 @@ using System.Text;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents one person.
+    /// </summary>
     public class PersonModel
     {
+        /// <summary>
+        /// First name of the person.
+        /// </summary>
         public String FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of the person.
+        /// </summary>
         public String LastName { get; set; }
+
+        /// <summary>
+        /// Email address of the person.
+        /// </summary>
         public String EmailAddress { get; set; }
+
+        /// <summary>
+        /// Cell phone number of the person.
+        /// </summary>
         public String CellPhoneNumber { get; set; }
     }
 }
