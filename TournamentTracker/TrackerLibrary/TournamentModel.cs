efa13b9cf5d5ecd0872783a4dedb1b766d323f30
DavidTran <dtrancpp@gmail.com>
2021-09-13 21:52:51 -0700

    add properties to TournamentModel

--- TournamentTracker/TrackerLibrary/TournamentModel.cs
+++ TournamentTracker/TrackerLibrary/TournamentModel.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace TrackerLibrary
 {
-    class TournamentModel
+    public class TournamentModel
     {
+        public String TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
     }
 }
